daedc293ddcaa0c24d33a4abd3c366793ac96479
tanya-kostecki <tanya.pavlova1294@gmail.com>
2025-08-26 21:32:11 +0200

    update index.js file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 4545;
  
 app.get('/ping', (req, res) => {
-  res.send({ message: 'pong' });
+  res.send({ message: 'pong', version: '1.0.0' });
 });
  
 app.listen(PORT, () => {
